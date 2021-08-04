e038b7d3166fe2bf1340f7de0b2c02e1c9a8991f
worasit <worasit@users.noreply.github.com>
2021-08-04 12:32:25 +0700

    Added log for the start point.

--- src/main/java/uk/tw/energy/joienergy/JoiEnergyApplication.java
+++ src/main/java/uk/tw/energy/joienergy/JoiEnergyApplication.java
@@ -1,12 +1,15 @@
 package uk.tw.energy.joienergy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JoiEnergyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JoiEnergyApplication.class, args);
+    log.info("Application is successfully started \uD83D\uDE80");
   }
 }
