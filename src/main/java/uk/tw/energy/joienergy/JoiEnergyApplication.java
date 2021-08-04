4933b4631b61a3e94969c84efe582a2dba222ae7
worasit <worasit@users.noreply.github.com>
2021-08-04 12:30:41 +0700

    Ignore gradle and intellij

---
+++ src/main/java/uk/tw/energy/joienergy/JoiEnergyApplication.java
@@ -0,0 +1,12 @@
+package uk.tw.energy.joienergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoiEnergyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JoiEnergyApplication.class, args);
+  }
+}
---
+++ src/test/java/uk/tw/energy/joienergy/JoiEnergyApplicationTests.java
@@ -0,0 +1,11 @@
+package uk.tw.energy.joienergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoiEnergyApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
