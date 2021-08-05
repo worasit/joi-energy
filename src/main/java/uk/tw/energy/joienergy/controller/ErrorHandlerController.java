04da69399d541a4d2c23e25123454670136d4f65
worasit <worasit@users.noreply.github.com>
2021-08-05 23:21:08 +0700

    Added custom error.

--- src/main/java/uk/tw/energy/joienergy/controller/ErrorHandlerController.java
+++ src/main/java/uk/tw/energy/joienergy/controller/ErrorHandlerController.java
@@ -1,19 +1,33 @@
 package uk.tw.energy.joienergy.controller;
 
+import java.time.LocalDate;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import uk.tw.energy.joienergy.dto.ErrorDto;
 import uk.tw.energy.joienergy.exception.MeterDuplicatedException;
+import uk.tw.energy.joienergy.exception.MeterNotFoundException;
 
 @ControllerAdvice
 public class ErrorHandlerController extends ResponseEntityExceptionHandler {
 
-  @ResponseBody
   @ExceptionHandler(MeterDuplicatedException.class)
-  ResponseStatusException meterDuplicatedHandler(MeterDuplicatedException ex) {
-    return new ResponseStatusException(HttpStatus.EXPECTATION_FAILED, ex.getMessage());
+  public ResponseEntity<ErrorDto> meterDuplicatedHandler(MeterDuplicatedException ex) {
+    final ErrorDto errorDto = ErrorDto.builder().httpStatus(HttpStatus.CONFLICT)
+        .time(LocalDate.now())
+        .message(ex.getMessage()).build();
+    return ResponseEntity.internalServerError().body(errorDto);
+  }
+
+  @ResponseStatus(
+      value = HttpStatus.CONFLICT,
+      reason = "Meter not found"
+  )
+  @ExceptionHandler(MeterNotFoundException.class)
+  public void meterDuplicatedHandler(MeterNotFoundException ex) {
   }
 }
--- src/main/java/uk/tw/energy/joienergy/dto/ErrorDto.java
+++ src/main/java/uk/tw/energy/joienergy/dto/ErrorDto.java
@@ -1,3 +1,15 @@
 package uk.tw.energy.joienergy.dto;
 
-public class ErrorDto {}
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Builder
+@Data
+public class ErrorDto {
+
+  private String message;
+  private LocalDate time;
+  private HttpStatus httpStatus;
+}
