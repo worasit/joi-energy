eed47847ac7ab99eb916aaa7f71a845829e374ba
worasit <worasit@users.noreply.github.com>
2021-08-05 23:38:17 +0700

    Added implementation of recommendation.

--- src/main/java/uk/tw/energy/joienergy/controller/PricePlanComparatorController.java
+++ src/main/java/uk/tw/energy/joienergy/controller/PricePlanComparatorController.java
@@ -1,17 +1,17 @@
 package uk.tw.energy.joienergy.controller;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import uk.tw.energy.joienergy.exception.MeterNotFoundException;
 import uk.tw.energy.joienergy.service.AccountService;
 import uk.tw.energy.joienergy.service.PricePlanService;
@@ -50,9 +50,19 @@ public class PricePlanComparatorController {
   }
 
   @GetMapping("/recommend/{smartMeterId}")
-  public ResponseEntity<List<Entry<String, BigDecimal>>>
+  public ResponseEntity<Map<String, BigDecimal>>
   recommendCheapestPricePlans(@PathVariable String smartMeterId,
       @RequestParam(name = "limit", required = false) Integer limit) {
-    throw new NotImplementedException();
+
+    final Map<String, BigDecimal> pricePlans = pricePlanService
+        .getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId)
+        .orElseThrow(() -> new MeterNotFoundException(smartMeterId));
+
+    final Map<String, BigDecimal> recommendedPricePlan = pricePlans.entrySet().stream()
+        .sorted(Entry.comparingByValue(Comparator.naturalOrder()))
+        .limit(limit)
+        .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+    return ResponseEntity.ok(recommendedPricePlan);
   }
 }
