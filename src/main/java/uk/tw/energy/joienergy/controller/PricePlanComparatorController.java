04f3cb8d21c54dee5298f4af6c587dd6077c2da6
worasit <worasit@users.noreply.github.com>
2021-08-05 22:52:51 +0700

    Added implementation of the pricePlanService for compare all pricePlan.

--- src/main/java/uk/tw/energy/joienergy/controller/PricePlanComparatorController.java
+++ src/main/java/uk/tw/energy/joienergy/controller/PricePlanComparatorController.java
@@ -1,6 +1,7 @@
 package uk.tw.energy.joienergy.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,14 +12,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import uk.tw.energy.joienergy.exception.MeterNotFoundException;
+import uk.tw.energy.joienergy.service.AccountService;
+import uk.tw.energy.joienergy.service.PricePlanService;
 
 @RestController
-@RequestMapping("/price-plan")
+@RequestMapping("/price-plans")
 public class PricePlanComparatorController {
 
+
+  public static final String PRICE_PLAN_ID = "pricePlanId";
+  public static final String PRICE_PLAN_COMPARISONS = "pricePlanComparisons";
+  private final AccountService accountService;
+  private final PricePlanService pricePlanService;
+
+  public PricePlanComparatorController(
+      AccountService accountService, PricePlanService pricePlanService) {
+    this.accountService = accountService;
+    this.pricePlanService = pricePlanService;
+  }
+
   @GetMapping("/compare-all/{smartMeterId}")
-  public ResponseEntity calculatedCostForEachPricePlan(@PathVariable String smartMeterId) {
-    throw new NotImplementedException();
+  public ResponseEntity<Map<String, Object>> calculatedCostForEachPricePlan(
+      @PathVariable String smartMeterId) {
+    final String pricePlanId = accountService
+        .getPricePlanIdForSmartMeterId(smartMeterId);
+
+    final Map<String, BigDecimal> comparedPricePlan = pricePlanService
+        .getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId)
+        .orElseThrow(() -> new MeterNotFoundException(smartMeterId));
+
+    final HashMap<String, Object> comparedResult = new HashMap<>();
+    comparedResult.put(PRICE_PLAN_ID, pricePlanId);
+    comparedResult.put(PRICE_PLAN_COMPARISONS, comparedPricePlan);
+
+    return ResponseEntity.ok(comparedResult);
   }
 
   @GetMapping("/recommend/{smartMeterId}")
--- src/test/java/uk/tw/energy/joienergy/service/PricePlanServiceTest.java
+++ src/test/java/uk/tw/energy/joienergy/service/PricePlanServiceTest.java
@@ -1,14 +1,23 @@
 package uk.tw.energy.joienergy.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Optional;
+import java.util.List;
+import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.tw.energy.joienergy.domain.ElectricityReading;
 
+@SpringBootTest
 class PricePlanServiceTest {
+  @Autowired
+  private List pricePlans;
+  @Autowired
+  private PricePlanService pricePlanService;
+  @Autowired
+  private MeterReadingService meterReadingService;
 
   @Test
   void getConsumptionCostOfElectricityReadingsForEachPricePlan() {
@@ -21,5 +30,18 @@ class PricePlanServiceTest {
 //
 //    // Assert
 //    assertThat(pricePlans).isNotEmpty();
+  }
+
+  @Test
+  void getAvgReading() {
+    // Arrange
+    final List<ElectricityReading> electricityReadings = meterReadingService
+        .getReadings("smart-meter-0").get();
+
+    // Act
+    final BigDecimal averageReadings = pricePlanService.calculateAverageReadings(electricityReadings);
+
+    // Assert
+    assertThat(averageReadings).isCloseTo(BigDecimal.ONE, Percentage.withPercentage(1));
   }
 }
